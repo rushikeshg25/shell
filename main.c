465d28e2a512bac0c2527ab5512a67c384d8ccf2
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2026-02-14 00:01:49 +0530

    feat: implement basic REPL loop with GNU Readline

--- main.c
+++ main.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include "execute.h"
+#include "signals.h"
 
 #define SHELL_PROMPT "shell> "
 
-int main() {
+void start_repl() {
     char *input;
     while (1) {
         input = readline(SHELL_PROMPT);
+        
         if (input == NULL) {
             printf("\n");
             break;
         }
+        
         if (strlen(input) > 0) {
             add_history(input);
-            printf("You entered: %s\n", input);
+            if (execute(input) == 0) {
+                free(input);
+                break;
+            }
         }
+        
         free(input);
     }
+}
+
+int main() {
+    setup_signals();
+    start_repl();
     return 0;
 }
